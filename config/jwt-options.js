e8f7a6b290f2f44ab9fd2a1d8fb2d0b812d35ede
Khem <khemrajsharmawh@gmail.com>
2018-01-05 16:15:52 +0530

    Initial Commit

---
+++ config/jwt-options.js
@@ -0,0 +1,8 @@
+/**
+ * Created by crosp on 5/13/17.
+ */
+module.exports = {
+    issuer: "arvind@web-hike.com",//"api@crosp.net",
+    audience: "www.web-hike.com",//"api.blog.crosp.net",
+    algorithm: "RS256"
+};
